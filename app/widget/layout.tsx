b0fa99453be8cf966229650c8a22da8e4d63de9b
Jayzuss1023 <jesus.flo369@gmail.com>
2026-09-23 15:29:46 -0500

    pull appId from params

--- app/widget/layout.tsx
+++ app/widget/layout.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import type { Viewport } from "next";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { WidgetProvider } from "./WidgetProvider";
+import { Id } from "@/convex/_generated/dataModel";
 
 export const viewport: Viewport = {
   width: "device-width",
@@ -13,5 +16,14 @@ export default function WidgetLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const [appId, setAppId] = useState<Id<"workspaces"> | null>(null);
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get("app_id");
+    if (!appId) {
+      setAppId(id as Id<"workspaces">);
+    }
+  });
+
   return <WidgetProvider>{children}</WidgetProvider>;
 }
